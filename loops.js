fcd7fc425c77a5906aecf545ba9941d6d21b5d08
agiratech-nikkel <nikkel.r@agiratech.com>
2022-05-18 19:32:20 +0530

    Hackerrank Task

--- loops.js
+++ loops.js
@@ -1,15 +1,12 @@
 function vowelsAndConsonants(s) {
-    let c, d;
-    for (let i = 0; i <= s.length - 1; i++) {
-        c = s[i];
-        if (c == "a" || c == "e" || c == "i" || c == "o" || c == "u") {
-            console.log(c)
+    const v = 'aeiou';
+    var c = '';
+    for (var i = 0; i < s.length; i++) {
+        if (v.indexOf(s[i]) > -1) {
+            console.log(s[i]);
+        } else {
+            c += s[i] + '\n';
         }
     }
-    for (let i = 0; i <= s.length - 1; i++) {
-        d = s[i];
-        if (d == "a" || d == "e" || d == "i" || d == "o" || d == "u") {} else {
-            console.log(d)
-        }
-    }
+    console.log(c);
 }
