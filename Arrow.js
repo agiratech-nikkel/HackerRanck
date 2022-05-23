a97f98c57439a1bc52443c59223eb63b6b8ee7fd
agiratech-nikkel <nikkel.r@agiratech.com>
2022-05-23 16:16:00 +0530

    HackerRank Day:-5

--- Arrow.js
+++ Arrow.js
@@ -2,3 +2,15 @@ function modifyArray(nums) {
     let ans = nums.map(num => num * (num % 2 === 0 ? 2 : 3));
     return ans
 }
+
+// function modifyArray(nums) {
+// init =0 
+// return nums.reduce((target, num,init) => {
+// target.push(
+// num % 2 === 0
+// ? num * 2
+// : num * 3
+// );
+// return target;
+// },[])
+// }
